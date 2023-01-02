aa500a8206f79ce34d05319c88b04237bb9047dc
Ahmed Hisham <ahmedhishamkamal@icloud.com>
2023-01-03 00:03:47 +0200

    Enhancing Storage Setting

--- MVC/index.php
+++ MVC/index.php
@@ -1,8 +1,11 @@
 <?php
 require 'vendor/autoload.php';
 
-use Ahmed\Mvc\Env\Env;
+use Ahmed\Mvc\Cookies\Cookies;
 use Ahmed\Mvc\Database\DB;
+use Ahmed\Mvc\Env\Env;
+use Ahmed\Mvc\Session\Session;
+use Ahmed\Mvc\Storage\Storage;
 
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
@@ -21,3 +24,11 @@ $whoops->register();
 
 
 echo Env::env("DB_HOST");
+// $session->set($key, $value);
+
+
+// $cookie = new Cookies;
+
+
+// $cookie->set('hello', "Val", time() + 60 * 60 * 24 * 7 * 30 * 12, '/');
+// $cookie->destroy('hello');
